72749c4ec14920f85e74f0aba4b42532b2b4ef41
s0ci4lx <55864073+s0ci4lx@users.noreply.github.com>
2025-04-16 22:59:01 +0700

    fix puppeteer for render

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 const ejs = require("ejs");
 const fs = require("fs");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,13 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/generate-pdf", async (req, res) => {
   const data = req.body;
 
-  // โหลด HTML template
   const templatePath = path.join(__dirname, "template.ejs");
   const html = await ejs.renderFile(templatePath, { data });
 
-  // ใช้ puppeteer แปลง HTML เป็น PDF
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    defaultViewport: chromium.defaultViewport,
   });
 
   const page = await browser.newPage();
